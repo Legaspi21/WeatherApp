6842cc066ccc32b5302b02c87d76a088af01ac38
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-01-14 00:36:02 -0800

    Changed temperature units from Kelvin to Fahrenheit

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,7 +6,7 @@ import GoogleMap from '../components/google_map'
 class WeatherList extends Component {
 	renderWeather(cityData) {
 		const name = cityData.city.name;
-		const temps = cityData.list.map(weather => weather.main.temp);
+		const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => (temp - 273)*1.8 + 32);
 		const pressures = cityData.list.map(weather => weather.main.pressure);
 		const humidity = cityData.list.map(weather => weather.main.humidity);
 		const { lon, lat } = cityData.city.coord;
@@ -14,7 +14,7 @@ class WeatherList extends Component {
 		return (
 				<tr key={name}>
 					<td><GoogleMap lon={lon} lat={lat}/></td>
-					<td><Chart data={temps} color="orange" units="K" /></td>
+					<td><Chart data={temps} color="orange" units={[<span>°</span>, 'F']} /></td>
 					<td><Chart data={pressures} color="green" units="hPa" /></td>
 					<td><Chart data={humidity} color="black" units="%" /></td>
 				</tr>
@@ -26,7 +26,7 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature (K)</th>
+						<th>Temperature ° F</th>
 						<th>Pressure (hPa)</th>
 						<th>Humidity (%)</th>
 					</tr>
